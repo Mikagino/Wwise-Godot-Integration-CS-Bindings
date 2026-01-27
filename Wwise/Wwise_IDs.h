2df1142f155da1599c0b64200909fdac6e1ce25d
Mikagino <angeli.mika@gmail.com>
2026-01-27 22:36:30 +0100

    generated test soundbanks

---
+++ Wwise/Wwise_IDs.h
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BOARD_IMPACT = 2797961412U;
+        static const AkUniqueID BOARD_JUMP = 2598381242U;
+        static const AkUniqueID BOARD_SLIDE = 939839769U;
+        static const AkUniqueID BOARD_SPARKS = 2847367608U;
+        static const AkUniqueID BUTTON_CLICK = 814543256U;
+        static const AkUniqueID BUTTON_HOVER = 1701177942U;
+        static const AkUniqueID CARGO_COLLECTED = 567910595U;
+        static const AkUniqueID CARGO_DELIVERED = 826022782U;
+        static const AkUniqueID CARLO_START = 3007153047U;
+        static const AkUniqueID DEATH_STINGER = 1015288770U;
+        static const AkUniqueID MISSION_DONE = 587014128U;
+        static const AkUniqueID SPEED_WIND = 1545604691U;
+        static const AkUniqueID START_GAME = 1114964412U;
+        static const AkUniqueID START_MUSIC = 540993415U;
+        static const AkUniqueID TEXT_BOOP = 1644585539U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace CURRENTMENU
+        {
+            static const AkUniqueID GROUP = 3730238669U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace CURRENTMENU
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MASTER_VOLUME = 4179668880U;
+        static const AkUniqueID MUFFLER = 4200322836U;
+        static const AkUniqueID MUSIC_VOLUME = 1006694123U;
+        static const AkUniqueID PLAYER_SPEED = 1062779386U;
+        static const AkUniqueID SOUND_VOLUME = 495870151U;
+        static const AkUniqueID UI_VOLUME = 1719345792U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ALLSOUNDS = 3278463154U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_BUS = 2680856269U;
+        static const AkUniqueID SOUND_BUS = 2910858937U;
+        static const AkUniqueID UI_BUS = 3247222208U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
